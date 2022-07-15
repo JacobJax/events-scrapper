6287278c54b718381d0cf556464284290f102080
JacobJax <jackjax617@gmail.com>
2022-07-15 13:29:21 +0300

    :sparkles: add database
    persist scrapped data to a mongoDB database

--- eventScrapper/eventScrapper/pipelines.py
+++ eventScrapper/eventScrapper/pipelines.py
@@ -7,7 +7,25 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import pymongo
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# load environment variables
+load_dotenv()
+
+CONN = os.getenv("DB_CONN_STRING")
+DB = os.getenv("DB_NAME")
+COLL = os.getenv("DB_COLLECTION")
 
 class EventscrapperPipeline:
+
+    def __init__(self):
+        self.cluster = MongoClient(f"{CONN}")
+        db = self.cluster[f"{DB}"]
+        self.collection = db[f"{COLL}"]
+
     def process_item(self, item, spider):
+        self.collection.insert_one(dict(item))
         return item
